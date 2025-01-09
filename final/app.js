3bafbe895cbafe6aa692b2afbfcdeb587268b2f9
Tesfanesh Asefa <126299722+Tesfa926@users.noreply.github.com>
2025-01-09 21:39:44 +0300

    Delete final directory

--- final/app.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navBar = document.querySelector('.nav-bar');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navBar.classList.toggle('active');
-});
